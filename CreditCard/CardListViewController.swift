85ec0d4aa0831736efe7aafd0194ab1388c6a224
joensumin <soomis7@naver.com>
2022-01-12 09:17:24 +0900

    FirebaseStore 읽기 / 쓰기 / 삭제

--- CreditCard/CardListViewController.swift
+++ CreditCard/CardListViewController.swift
@@ -8,39 +8,64 @@
 import UIKit
 import Kingfisher
 import FirebaseDatabase
+import FirebaseFirestore
 
 class CardListViewController: UITableViewController {
-
+    
     var creditCardList: [CreditCard] = []
-    var ref: DatabaseReference! // Firebase Realtime Database
+    //    var ref: DatabaseReference! // Firebase Realtime Database
+    var db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //UITableView Cell Register
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
         
-        ref = Database.database().reference()
-        ref.observe(.value){ snapshot in
-            //snapshot value 는 [String:[String:Any]] 형태의 딕셔너리 타입
-            
-            guard let value = snapshot.value as? [String:[String:Any]] else { return }
-            
-            do{
-                
-                let jsonData = try JSONSerialization.data(withJSONObject: value) //json으로 포맷으로 변환
-                let cardData = try JSONDecoder().decode([String : CreditCard].self, from: jsonData)
-                let cardList = Array(cardData.values)
-                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
-                
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            } catch let error {
-                print("ERROR JSON parsing \(error.localizedDescription)")
+        //        실시간 데이터베이스 읽기
+        //        ref = Database.database().reference()
+        //        ref.observe(.value){ snapshot in
+        //            //snapshot value 는 [String:[String:Any]] 형태의 딕셔너리 타입
+        //
+        //            guard let value = snapshot.value as? [String:[String:Any]] else { return }
+        //
+        //            do{
+        //
+        //                let jsonData = try JSONSerialization.data(withJSONObject: value) //json으로 포맷으로 변환
+        //                let cardData = try JSONDecoder().decode([String : CreditCard].self, from: jsonData)
+        //                let cardList = Array(cardData.values)
+        //                self.creditCardList = cardList.sorted { $0.rank < $1.rank }
+        //
+        //                DispatchQueue.main.async {
+        //                    self.tableView.reloadData()
+        //                }
+        //            } catch let error {
+        //                print("ERROR JSON parsing \(error.localizedDescription)")
+        //            }
+        //
+        //        }
+        
+        //Firestore 읽기
+        db.collection("creditCardList").addSnapshotListener { snapshot, error in
+            guard let documents = snapshot?.documents else {
+                print("ERROR Firebase fetching document \(String(describing: error))")
+                return
             }
+            self.creditCardList = documents.compactMap { doc -> CreditCard? in
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
+                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
+                    return creditCard
+                }catch {
+                    print("ERROR JSON Parsing \(error)")
+                    return nil
+                }
+            }.sorted{ $0.rank < $1.rank}
             
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -68,37 +93,69 @@ class CardListViewController: UITableViewController {
         self.show(detailViewController, sender: nil )
         
         //fireBase 값 수정 방법_Option1
-        let cardID = creditCardList[indexPath.row].id
-//        ref.child("Item\(cardID)/isSelected").setValue(true)
+        //        let cardID = creditCardList[indexPath.row].id
+        //        ref.child("Item\(cardID)/isSelected").setValue(true)
         
         //fireBase 값 수정 방법_Option2
-        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
-            guard let self = self ,
-                  let value = snapshot.value as? [String:[String:Any]],
-                  let key = value.keys.first  else { return }
-            self.ref.child("\(key)/isSelected").setValue(true)
+        //        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+        //            guard let self = self ,
+        //                  let value = snapshot.value as? [String:[String:Any]],
+        //                  let key = value.keys.first  else { return }
+        //            self.ref.child("\(key)/isSelected").setValue(true)
+        //        }
+        
+        //       Firebase 쓰기
+        //       optin 1 (databased의 컬렉션 경로를 아는경우 )
+        let cardID = creditCardList[indexPath.row].id
+//        db.collection("creditCardList").document("card\(cardID)").updateData(["isSelected": true])
+        
+        //        option 2 (databse의 컬렉션 경로를 모를 경우)
+        db.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments { snapshot, error in
+            //snapshot은 배열로 전달
+            guard let document = snapshot?.documents.first else {
+                print("ERROR Firestore fetching docment")
+                return
+            }
+            document.reference.updateData(["isSelected": true])
         }
+        
+        
     }
     
-     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            //실시간 데이터베이스 삭제
             //fireBase Database value remove Option1
-            let cardID = creditCardList[indexPath.row].id
-            ref.child("Item\(cardID)").removeValue()
+            //            let cardID = creditCardList[indexPath.row].id
+            //            ref.child("Item\(cardID)").removeValue()
             
             //fireBase Database value remove Option2
-            ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
-                guard let self = self,
-                      let value = snapshot.value as? [String:[String:Any]],
-                      let key = value.keys.first else { return }
-                
-                self.ref.child(key).removeValue()
+            //            ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+            //                guard let self = self,
+            //                      let value = snapshot.value as? [String:[String:Any]],
+            //                      let key = value.keys.first else { return }
+            //
+            //                self.ref.child(key).removeValue()
+            //            }
+            
+            //firestore 삭제
+            //option 1 (데이터의 경로를 알고 있을때)
+            let cardID = creditCardList[indexPath.row].id
+//            db.collection("creditCardList").document("card\(cardID)").delete()
+            
+            //option 2 (데이터의 경로를 모르고 있을때)
+            db.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments { snapshot, error in
+                guard let document = snapshot?.documents.first else {
+                    print("ERROR Firestore fetching document")
+                    return
+                }
+                document.reference.delete()
             }
         }
     }
---
+++ README.md
@@ -0,0 +1,37 @@
+# CreditCard
+카드 추천 앱
+
+## 기능 상세
+- 카드 추천 리스트를 볼 수 있다. 
+- 해당 카드의 혜택 상세 내용을 볼 수 있다.
+- 해당 카드를 삭제 할 수 있다. 
+ 
+
+## 활용기술 
+- Firebase Database 
+- Firebase Storage
+- Kingfisher 
+- lottie
+
+
+## DEMO
+
+![ezgif com-gif-maker](https://user-images.githubusercontent.com/51107183/149041117-1f6811ba-ae89-4b5b-90f4-548c1f46f29f.gif)
+
+
+## 배운 내용
+
+#### Firebase RealTime Database 
+> 비관계형 클라우드 데이터베이스
+> 
+> NoSQL 기반 Cloud-hosted database 로 실시간으로 모든 클라우드에서 데이터가 동기화 되고 앱이 오프라인 상태라도 계속 사용 
+
+#### RealTime Database  & Cloud Firebase  
+
+![스크린샷 2022-01-11 오후 4 54 08](https://user-images.githubusercontent.com/51107183/148902623-886910c4-8928-4079-b0e5-57c3119ddb71.png)
+
+![스크린샷 2022-01-11 오후 4 54 43](https://user-images.githubusercontent.com/51107183/148902673-26e612c4-e4f6-4f7b-8319-00fc7fe1c55f.png)
+
+
+
+
